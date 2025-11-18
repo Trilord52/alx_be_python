10b6756e4e77b89ebf1c18944ec57b31441afe69
Trilord52 <tinbiteyonas52@gmail.com>
2025-11-18 18:18:45 +0300

    fix for arthemetic

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,4 +1,4 @@
-from arithmetic_operations import perform_function
+from arithmetic_operations import perform_operation
 
 def main():
     print("Arithmetic Operations")
@@ -6,7 +6,7 @@ def main():
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-    result = perform_function(num1, num2, operation)
+    result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
 
 if __name__ == "__main__":
