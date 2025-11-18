a6974c73819c99b27035978afd7b31ee498b484d
Trilord52 <tinbiteyonas52@gmail.com>
2025-11-18 17:14:32 +0300

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_function(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1- num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "num2 cannot be zero"
+        return num1 / num2
