6152a0b3036e487076da119cd070a72f56369a8c
Trilord52 <tinbiteyonas52@gmail.com>
2025-12-14 20:40:07 +0300

    polymorphism

--- oop/main.py
+++ oop/main.py
@@ -1,3 +1,5 @@
+# For task 0
+
 from book_class import Book
 
 def main():
@@ -21,7 +23,7 @@ if __name__ == "__main__":
     
     
     
-    
+#For task 1    
 from library_system import Book, EBook, PrintBook, Library
 
 def main():
@@ -40,6 +42,29 @@ def main():
 
     # List all books in the library
     my_library.list_books()
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+# For task 2    
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
+
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
 if __name__ == "__main__":
     main()
